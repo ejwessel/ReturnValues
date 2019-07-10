1095861f2d3af051f62abab1c83cdae029d5a618
Ethan Wessel <ethan.wessel@fluidity.io>
2019-07-10 13:33:09 -0400

    more tests

--- contracts/StatefulContract.sol
+++ contracts/StatefulContract.sol
@@ -1,12 +1,27 @@
 pragma solidity ^0.5.0;
 
 contract StatefulContract {
-  uint256 public uintData;
+  uint256 public data;
 
   /**
-  * setUintData will set the uint data to a new value
+  * setData will set the uint data to a new value
   */
-  function setUintData(uint256 newUintData) external returns (bool){
-      uintData = newUintData;
+  function setData(uint256 newData) external {
+      data = newData;
+  }
+
+  /**
+  * Takes the current data value, adds 1 and returns it
+  */
+  function getDataPlus1() external view returns (uint) {
+      return data + 1;
+  }
+
+  /**
+  * Modifies state and returns the current data value
+  */
+  function modifyStateAndReturnNewVal(uint256 newData) external returns (uint) {
+      data = newData;
+      return data;
   }
 }
--- test/StatefulContract.test.js
+++ test/StatefulContract.test.js
@@ -14,9 +14,34 @@ contract('StatefulContract', async (accounts) =>  {
         await helper.revertToSnapShot(snapshotId);
     });
 
-    it("Check State after initialization", async() => {
+    it("Example of a Call", async() => {
+      await statefulContract.setData.call(5)
+      let data = await statefulContract.data.call()
+      assert.equal(data.toNumber(), 0)
     });
 
-    it("Change State", async() => {
+    it("Example of a Transaction", async() => {
+      await statefulContract.setData(5)
+      let data = await statefulContract.data.call()
+      assert.equal(data.toNumber(), 5)
+    });
+
+    it("Example of getDataPlus1 using call", async() => {
+      await statefulContract.setData(20)
+      let data = await statefulContract.getDataPlus1.call()
+      assert.equal(data.toNumber(), 21)
+    });
+
+    it("Example of getDataPlus1 using transaction", async() => {
+      await statefulContract.setData(20)
+      let data = await statefulContract.getDataPlus1()
+      assert.equal(data.toNumber(), 21)
+    });
+
+    it("Example of modifyStateAndReturnNewVal using transaction", async() => {
+      let data = await statefulContract.modifyStateAndReturnNewVal.call(14)
+      await statefulContract.modifyStateAndReturnNewVal(14)
+      let data = await statefulContract.modifyStateAndReturnNewVal.call(14)
+      assert.equal(data.toNumber(), 14)
     });
 });
